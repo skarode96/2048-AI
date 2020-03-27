40a6177887d67e89fd23e30453889be4c7a4b100
skarode96 <skarode96@gmail.com>
2020-03-27 01:15:51 +0000

    refactoring function names

---
+++ 2048/test_main.py
@@ -0,0 +1,9 @@
+from unittest import TestCase
+import main
+
+class Test(TestCase):
+    def test_greedy_algo(self):
+        main.greedy_algo()
+
+    def test_random_algo(self):
+        main.random_algo()
